b1947c359dd6cc3fe0ddd6e4977b113081b84bec
Joemollytana <andrey-shkolnik@protonmail.com>
2020-06-18 09:06:30 +0200

    Update initDB.php
    comment lel

--- Public/initDB.php
+++ Public/initDB.php
@@ -2,7 +2,12 @@
 
 require 'rb.php';
 
-R::setup('mysql:host=localhost; dbname=DATENBANKNAME', 'USER', 'PWD');
+R::setup('mysql:host=localhost; dbname=DATENBANKNAME', 'USER', 'PWD'); //masteruser ?
 
+$a = R::dispense('usertabelle');  //usertabelle wird erstellen
+$a->name = "testuser";
+$a->password = ""; //sha2 boi?
+$a = R::store($a);
 R::close();
+R::freeze(true);
  ?>
