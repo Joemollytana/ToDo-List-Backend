28e4282f2e24719cfd05c82f6c5728cc7d8a0ad4
David Bährens <d.baehrens@web.de>
2020-06-18 22:44:08 +0200

    update initDb & Datenhaltung, hash password

--- Public/initDB.php
+++ Public/initDB.php
@@ -21,38 +21,44 @@ $tasklist2 = R::dispense('tasklist');
 
 /* Usertable */
 $user->username = "test";
-$user->password = "asdf";
+$user->password = password_hash("asdf", PASSWORD_DEFAULT);
 
 
 /* Tasks */
 $task1->taskname = "Wash dishes";
 $task1->description = "I have to wash my dishes.";
 $task1->scope = "large";
+$task1->deadline = NULL;
 $task1->done = FALSE;
 
 $task2->taskname = "Develop Web-App";
 $task2->description = "I have to develop a Web-App with PHP.";
 $task2->scope = "large";
+$task2->deadline = date_create("2020-07-01");
 $task2->done = FALSE;
 
 $task3->taskname = "Clean my desk";
 $task3->description = "I need to clean my desk, before I start to develop the Web-App.";
 $task3->scope = "small";
+$task3->deadline = date_create("2020-06-17");
 $task3->done = TRUE;
 
 $task4->taskname = "Lawn mowing";
 $task4->description = "My lawn looks like a mess.";
-$task4->scope = "medium";
+$task4->scope = "NULL";
+$task4->deadline = date_create("2020-06-28");
 $task4->done = TRUE;
 
 $task5->taskname = "Study for the exam";
 $task5->description = "I have to learn.";
 $task5->scope = "very large";
+$task5->deadline = date_create("2020-06-15");
 $task5->done = TRUE;
 
 $task6->taskname = "tax declaration";
 $task6->description = "I need to do my tax declaration till 31.07.";
 $task6->scope = "medium";
+$task6->deadline = date_create("2020-07-31");
 $task6->done = FALSE;
 
 
@@ -73,6 +79,9 @@ $tasklist2->xownTasksList[] = $task6;
 /* Store the tuples in database */
 $id = R::store($tasklist1);
 $id = R::store($tasklist2);
+
+/* Freeze database */
+R::freeze( TRUE );
 
 R::close();
 ?>
